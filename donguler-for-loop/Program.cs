328a848ce1065428fd0a061dd0925fdefe9df65c
Ramazan Kağan Yüksel <72298624+kaganyuksel99@users.noreply.github.com>
2021-10-30 15:18:00 +0300

    Add files via upload

---
+++ donguler-for-loop/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace donguler_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // // Ekrandan girilen sayıya kadar olan tek sayılarını ekrana yazdır. 
+            // Console.Write("Lütfen bir sayı giriniz"); 
+            // int sayac = int.Parse(Console.ReadLine());
+            // for (int i = 1; i <= sayac; i++)
+            // {   
+            //     // komutlar
+            //     if(i % 2 == 1){
+            //         Console.WriteLine(i);
+            //     }  
+                 
+            // } 
+            // // 1 ile 1000 arasındaki tek ve çift sayıların kendi içerisindeki toplamlarını ekrana yazdırma
+            // int tekToplam = 0;
+            // int ciftToplam = 0;
+            // for (int i = 1; i <= 1000; i++)
+            // {
+            //     if(i % 2 == 1) {
+            //         tekToplam += i; // tekToplam = tektoplam + i
+            //     } else { 
+            //         ciftToplam += i; // ciftToplam = cifttoplam + i
+            //     }
+            // }
+            // Console.WriteLine("Tek toplam =" + tekToplam); 
+            // Console.WriteLine("Çift toplam" + ciftToplam); 
+
+            // break , continue 
+            for (int i = 1; i < 10; i++)
+            {
+                if(i == 4)
+                    break; // 4'ü gördüğü gibi duru ve 4'ü yazmaz.
+                Console.WriteLine(i);
+            } 
+
+            for (int i = 1; i < 10; i++)
+            {
+                if(i == 4) // 4'ü gördüğünde onu atlar.
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
